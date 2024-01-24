0704b066be4e4432d82262a4be30a4ba4ebc4e2e
abduljalal14 <abdoeljalalexotic@gmail.com>
2024-01-24 21:19:33 +0700

    api 000webhost

--- lib/network/api.dart
+++ lib/network/api.dart
@@ -3,7 +3,7 @@ import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
 
 class Network {
-  final String _url = 'https://list-design-backend.000webhostapp.com/';
+  final String _url = 'https://list-design-backend.000webhostapp.com/api';
    // ignore: prefer_typing_uninitialized_variables
    var token;
 
